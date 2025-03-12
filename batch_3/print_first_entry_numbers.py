dfc736690bb07e8c36424bf1c0abfb7a302bdb69
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 02:03:32 +0800

    Created a loop prompting for ten numbers.

---
+++ batch_3/print_first_entry_numbers.py
@@ -0,0 +1,5 @@
+# Prompt user to enter 10 numbers and display all unique numbers. 
+# For numbers with duplicate, display only the first entry.
+
+for count in range(10):
+    num = float(input("Enter number " + str(count + 1) + ": "))
