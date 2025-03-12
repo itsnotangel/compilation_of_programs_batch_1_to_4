8aa77f506c83996732b57232bc0d6a2bef4fbb3a
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-12 18:22:56 +0800

    Updated code description and change variable names.

---
+++ batch_1/sum_of_two_numbers.py
@@ -0,0 +1,7 @@
+# Prompts the user for two numbers, adds them, and prints the result.
+
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number 2: "))
+sum_result = num1 + num2
+
+print(sum_result)
