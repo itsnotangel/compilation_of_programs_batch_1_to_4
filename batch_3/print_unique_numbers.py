a241eec81c2edfeb51cf81019406fd3d5a0d687d
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 01:23:57 +0800

    Created a list for all inputted numbers.

--- batch_3/print_unique_numbers.py
+++ batch_3/print_unique_numbers.py
@@ -1,4 +1,8 @@
 # A program that prints numbers without duplicates.
+num_list = []
 
 for count in range(10):
     num = float(input("Enter number " + str(count+1) + ": "))
+    num_list.append(num)
+
+print(num_list)
