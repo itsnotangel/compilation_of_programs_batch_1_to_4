1a7c83324b9733e3cd36c9710e98ce9d1fa63faa
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-12 17:46:50 +0800

    Updated variable name and output formatting.

---
+++ batch_1/print_bigger_number.py
@@ -0,0 +1,11 @@
+# Program: Find the Bigger Number Between Two Numbers
+
+num1 = float(input("Enter Number 1: ")) 
+num2 = float(input("Enter Number 2: "))
+
+if num1 > num2:
+    print(num1, "is the bigger number.")
+elif num1 < num2:
+    print(num2, "is the bigger number.")
+else:
+    print("The numbers are equal.")
