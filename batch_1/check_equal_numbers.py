c5e3654f3340a789de050ae3f23cfce7cfa4227b
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-12 18:05:51 +0800

    Updated the number comparison.

---
+++ batch_1/check_equal_numbers.py
@@ -0,0 +1,9 @@
+# Prompts the user for two numbers and checks if they are equal.
+
+num1 = float(input("Enter Number 1: ")) 
+num2 = float(input("Enter Number 2: "))
+
+if num1 == num2:
+    print("The numbers are equal.")
+else:
+    print("The numbers are not equal.")
