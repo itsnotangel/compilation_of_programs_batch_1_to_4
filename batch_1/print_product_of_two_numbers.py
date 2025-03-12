307d0e06061ac8ed58ff6c0afc7a9e3d7a7a16b8
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-12 18:50:10 +0800

    Changed variable names and improved code description.

---
+++ batch_1/print_product_of_two_numbers.py
@@ -0,0 +1,7 @@
+# Prompts the user for two numbers, multiplies them, and prints the result.
+
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number 2: "))
+product_result = num1 * num2
+
+print(product_result)
