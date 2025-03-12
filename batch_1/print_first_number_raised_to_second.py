8d50957a8cdf335d3a353ac93d6800eaa5c6c623
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-12 19:38:45 +0800

    Updated code description, improved variable names, and fixed spacing.

---
+++ batch_1/print_first_number_raised_to_second.py
@@ -0,0 +1,8 @@
+# Prompt user for two numbers and print the first number raised to the power of the second number.
+
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number 2: "))
+
+raised_result = num1 ** num2
+
+print("Result:", raised_result)
