a052e9e4256ddc2c0facc883adb1b9bdd903d04f
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-12 20:40:45 +0800

    Added code description and change output formatting.

---
+++ batch_2/check_if_not_equal_numbers.py
@@ -0,0 +1,9 @@
+# Prompts the user for two numbers, checks and print if they are either not equal or equal.
+
+num1 = float(input("Enter Number 1: ")) 
+num2 = float(input("Enter Number 2: "))
+
+if num1 == num2:
+    print("The numbers are equal.")
+else:
+    print("The numbers are not equal.")
