65cff0579410618e030aa44a17fe993955a8c4ae
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 02:06:12 +0800

    Created a list for entered number.

--- batch_3/print_first_entry_numbers.py
+++ batch_3/print_first_entry_numbers.py
@@ -1,5 +1,10 @@
 # Prompt user to enter 10 numbers and display all unique numbers. 
 # For numbers with duplicate, display only the first entry.
 
+num_list = []
 for count in range(10):
     num = float(input("Enter number " + str(count + 1) + ": "))
+    num_list.append(num)
+
+print(num_list)
+    
