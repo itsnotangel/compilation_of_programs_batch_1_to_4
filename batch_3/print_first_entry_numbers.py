6e732535c4a3890a3103d273c5cfcb1e0ec81575
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 02:12:50 +0800

    Filtered duplicate numbers and stored only the first number if duplicated.

--- batch_3/print_first_entry_numbers.py
+++ batch_3/print_first_entry_numbers.py
@@ -2,9 +2,12 @@
 # For numbers with duplicate, display only the first entry.
 
 num_list = []
+duplicates = []
 for count in range(10):
     num = float(input("Enter number " + str(count + 1) + ": "))
-    num_list.append(num)
+    if num not in duplicates:
+        num_list.append(num)
+        duplicates.append(num)
 
 print(num_list)
     
