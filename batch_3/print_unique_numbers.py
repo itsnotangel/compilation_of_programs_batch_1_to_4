a5c130a58a23fc96a3e28019b05a81526f1f0d4d
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 01:42:42 +0800

    Fixed input formatting and sapcing.

--- batch_3/print_unique_numbers.py
+++ batch_3/print_unique_numbers.py
@@ -1,10 +1,11 @@
 # A program that prints numbers without duplicates.
+
 num_list = []
 
 for count in range(10):
-    num = float(input("Enter number " + str(count+1) + ": "))
+    num = float(input("Enter number " + str(count + 1) + ": "))
     num_list.append(num)
 
 unique_num = [num for num in num_list if num_list.count(num) == 1]
 
-print(unique_num)
+print("Numbers without duplicates:", unique_num)
