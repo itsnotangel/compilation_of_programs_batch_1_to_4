699e0a027eea0506eaee95a8cd7868f12180506c
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-12 21:12:20 +0800

    Fixed spacing, improved variable names, and updated code description.

--- batch_2/print_quotient_without_decimal.py
+++ batch_2/print_quotient_without_decimal.py
@@ -1,4 +1,4 @@
-# Prompt user for two numbers and print the quotient rounded to two decimal places.
+# Prompt user for two numbers and print the quotient without decimal places.
 # If the second number is 0, print "Cannot divide by zero"
 
 num1 = float(input("Enter number 1: "))
@@ -7,5 +7,5 @@ num2 = float(input("Enter number 2: "))
 if num2 == 0:
     print("Cannot divide by zero.")
 else:
-    quotient = round(num1 / num2, 2)
+    quotient = num1 // num2
     print("Quotient:", quotient)
