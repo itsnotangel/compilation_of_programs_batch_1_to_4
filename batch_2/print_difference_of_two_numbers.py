c0ac4beb66955723ca4dd6f4c046fe91c7ccbc33
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-12 21:02:57 +0800

    Changed variable name and updated the code description.

---
+++ batch_2/print_difference_of_two_numbers.py
@@ -0,0 +1,8 @@
+# Prompt user to enter two numbers and print the difference of the two numbers.
+
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number 2: "))
+
+difference = num1 - num2
+
+print("Difference:", difference)
