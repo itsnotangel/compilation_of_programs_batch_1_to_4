28ba40df8d8937af5eff244f55a443f7b0155122
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 16:54:54 +0800

    Added a prompt asking user to input ten numbers.

---
+++ batch_4/print_all_duplicate_numbers.py
@@ -0,0 +1,5 @@
+# A program that ask user to input 10 numbers. 
+# Display all numbers that have duplicate.
+
+for count in range(10):
+    num = float(input("Enter number " + str(count + 1) + ": "))
