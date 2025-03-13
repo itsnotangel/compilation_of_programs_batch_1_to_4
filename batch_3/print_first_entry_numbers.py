79da5327664c17cdd66d5235a11f0a2dc7fcc7d7
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 21:16:33 +0800

    Removed seen_num since num_list already tracks unique numbers.

--- batch_3/print_first_entry_numbers.py
+++ batch_3/print_first_entry_numbers.py
@@ -2,12 +2,10 @@
 # For numbers with duplicate, display only the first entry.
 
 num_list = []
-seen_num = []
 
 for count in range(10):
     num = float(input("Enter number " + str(count + 1) + ": "))
-    if num not in seen_num:
+    if num not in num_list:
         num_list.append(num)
-        seen_num.append(num)
 
 print("The numbers are:", num_list)
