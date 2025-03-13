c57346d0af4f00e6c6de5bbbdcd88f7b91fb2de6
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 21:05:10 +0800

    Validates first if the inputted is duplicate otherwise, unique. Improved ValueError output formatting.

--- batch_3/check_unique_or_duplicate.py
+++ batch_3/check_unique_or_duplicate.py
@@ -1,16 +1,15 @@
-# A program that prompts the user to input a number and checks if it's unique or a duplicate. 
-# For invalid inputs, it prints "Invalid" and exits the program.
+# A program that prompts the user to input a number and checks if it's unique or a duplicate.
 
 num_list = []
 
 while True:
     try:
         num = float(input("Enter a number: "))
-        if num not in num_list:
+        if num in num_list:
+            print("Duplicate")
+        else:
             print("Unique")
             num_list.append(num)
-        else:
-            print("Duplicate")
     except ValueError:
-        print("End")
+        print("Invalid input.")
         break
