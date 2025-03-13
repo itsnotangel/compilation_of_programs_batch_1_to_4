476b4db53baeb4036bfac53b264a918bf02c354a
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 20:01:34 +0800

    Created a list for storing inputted numbers.

--- batch_4/number_with_most_duplicate.py
+++ batch_4/number_with_most_duplicate.py
@@ -1,4 +1,6 @@
 #  A program that keeps asking the user for a number until they enter an invalid input. Then, show the number that appeared the most.
 
+num_list = []
 num = float(input("Enter a number: "))
-print(num)
+num_list.append(num)
+print(num_list)
