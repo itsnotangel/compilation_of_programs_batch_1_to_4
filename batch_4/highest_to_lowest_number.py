58367050b4617bcf085ecc13965656656b0a47ce
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 18:52:41 +0800

    Added try-except to check if the inputted numbers are valid or not.

--- batch_4/highest_to_lowest_number.py
+++ batch_4/highest_to_lowest_number.py
@@ -1,6 +1,12 @@
 # A program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest.
 
 num_list = []
-num = float(input("Enter a number: "))
-num_list.append(num)
-print(num_list)
+
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        num_list.append(num)
+        print(num_list)
+    except ValueError:
+        print("Invalid")
+        break
