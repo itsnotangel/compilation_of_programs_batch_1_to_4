e0bfb7f129aafd18e2461b969c2a5607b8a33b30
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 17:26:11 +0800

    Added a prompt asking for a number.

---
+++ batch_4/find_highest_number.py
@@ -0,0 +1,6 @@
+# Create a program that ask user to input a number.
+# Continue asking until the user input is invalid. 
+# Display the highest number.
+
+num = float(input("Enter a number: "))
+print(num)
