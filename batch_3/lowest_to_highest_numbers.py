d938c0d20d98196e17f84cf3ef8d002d0c0406f0
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 14:13:29 +0800

    Added a prompt to ask user for a numebr and sucessfully listed the input number to a list.

---
+++ batch_3/lowest_to_highest_numbers.py
@@ -0,0 +1,7 @@
+# Aprogram that asks the user to input a number, continues asking until the user input is invalid. 
+# Displays the numbers from lowest to highest.
+
+num_list = []
+num = float(input("Enter a number: "))
+num_list.append(num)
+print(num_list)
