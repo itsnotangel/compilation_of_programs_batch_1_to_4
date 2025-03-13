1d5a4a2588388b35380e2c58157aa82accd52891
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 12:45:50 +0800

    Added a prompt for the user to enter a number and sucessfully listed the number entered.

---
+++ batch_3/check_unique_or_duplicate.py
@@ -0,0 +1,10 @@
+# A program that prompts the user to input a number and checks if it's unique or a duplicate. 
+# For invalid inputs, it prints "Invalid" and exits the program.
+
+num_list = []
+num = float(input("Enter a number: "))
+num_list.append(num)
+
+print(num_list)
+
+
