0514a92c9919e780200c8595c3178d0bdc20676f
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 17:09:47 +0800

    Added a logic to find and print the duplicated numbers.

--- batch_4/print_all_duplicate_numbers.py
+++ batch_4/print_all_duplicate_numbers.py
@@ -6,4 +6,7 @@ num_list = []
 for count in range(10):
     num = float(input("Enter number " + str(count + 1) + ": "))
     num_list.append(num)
-print(num_list)
+
+duplicate_num = list(set([num for num in num_list if num_list.count(num) > 1]))
+
+print(duplicate_num)
