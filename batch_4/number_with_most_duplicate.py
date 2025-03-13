71c789c770855a83a243943e7d3c69f2f5f9e682
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 22:19:30 +0800

    Change 'mode' to 'frequent_num' and fixed spacing.

--- batch_4/number_with_most_duplicate.py
+++ batch_4/number_with_most_duplicate.py
@@ -1,6 +1,7 @@
 #  A program that keeps asking the user for a number until they enter an invalid input. Then, show the number that appeared the most.
 
 num_list = []
+
 while True:
     try:
         num = float(input("Enter a number: "))
@@ -9,10 +10,11 @@ while True:
         break
 
 if num_list:
-    mode = max(set(num_list), key=num_list.count)
-    if num_list.count(mode) == 1:
+    frequent_num = max(set(num_list), key=num_list.count)
+
+    if num_list.count(frequent_num) == 1:
         print("No duplicates.")
     else:
-        print("The number with most duplicate:", mode)
+        print("The number with most duplicate:", frequent_num)
 else:
-    print("Invalid")
+    print("Invalid input.")
