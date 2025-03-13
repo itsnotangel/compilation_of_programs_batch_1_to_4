a8b666de75a954bf62702352690e6a2fbca41e3b
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 20:06:37 +0800

    Added try-except to filter out valid and invalid inputs.

--- batch_4/number_with_most_duplicate.py
+++ batch_4/number_with_most_duplicate.py
@@ -1,6 +1,10 @@
 #  A program that keeps asking the user for a number until they enter an invalid input. Then, show the number that appeared the most.
 
 num_list = []
-num = float(input("Enter a number: "))
-num_list.append(num)
-print(num_list)
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        num_list.append(num)
+    except ValueError:
+        print(num_list)
+        break
