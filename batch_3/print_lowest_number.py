e014e23936646583f0de4ae8af1df0bc5dd54c31
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 13:26:51 +0800

    Added a prompt to ask user for a number and sucessfully listed it.

---
+++ batch_3/print_lowest_number.py
@@ -0,0 +1,6 @@
+# A program that asks the user to input a number, continues asking until the input is invalid, and displays the lowest number.
+
+num_list = []
+num = float(input("Enter a number: "))
+num_list.append(num)
+print(num_list)
