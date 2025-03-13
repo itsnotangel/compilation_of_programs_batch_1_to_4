0fca05ff13d51d7c430d0a98d30f98a45be937e9
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 13:52:00 +0800

    Filtered out valid and invalid input.

--- batch_3/print_lowest_number.py
+++ batch_3/print_lowest_number.py
@@ -1,6 +1,11 @@
 # A program that asks the user to input a number, continues asking until the input is invalid, and displays the lowest number.
 
 num_list = []
-num = float(input("Enter a number: "))
-num_list.append(num)
-print(num_list)
+
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        num_list.append(num)
+    except ValueError:
+        print("Invalid")
+        break
