6565bf1b77a1bbc26e07d200a0cc544228764a4c
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 19:38:31 +0800

    Added try-except to filter out valid and invalid inputs.

--- batch_4/average_of_all_inputted_numbers.py
+++ batch_4/average_of_all_inputted_numbers.py
@@ -1,6 +1,10 @@
 # A program that repeatedly prompts the user for a number until an invalid input is entered, then displays the average.
 
 num_list = []
-num = float(input("Enter a number: "))
-num_list.append(num)
-print(num_list)
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        num_list.append(num)
+    except ValueError:
+        print(num_list)
+        break
