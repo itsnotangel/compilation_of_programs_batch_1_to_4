aef12c61e8b3981ec434b72c1438bdf7486112b1
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 19:34:33 +0800

    Created a list.

--- batch_4/average_of_all_inputted_numbers.py
+++ batch_4/average_of_all_inputted_numbers.py
@@ -1,4 +1,6 @@
 # A program that repeatedly prompts the user for a number until an invalid input is entered, then displays the average.
 
+num_list = []
 num = float(input("Enter a number: "))
-print(num)
+num_list.append(num)
+print(num_list)
