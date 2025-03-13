02a3f13aa574d35de1b5886ff48dc0ec87e1ed2f
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 18:48:39 +0800

    Changed 'int' to 'input'.

--- batch_4/highest_to_lowest_number.py
+++ batch_4/highest_to_lowest_number.py
@@ -1,4 +1,4 @@
 # A program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest.
 
-num = float(int("Enter a number: "))
+num = float(input("Enter a number: "))
 print(num)
