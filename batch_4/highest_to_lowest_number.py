eecadd87f65814554b566ab100225351afad12d4
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 18:44:04 +0800

    Added a prompt asking for a number.

---
+++ batch_4/highest_to_lowest_number.py
@@ -0,0 +1,4 @@
+# A program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest.
+
+num = float(int("Enter a number: "))
+print(num)
