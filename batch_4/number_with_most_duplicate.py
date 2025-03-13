ca4e41c76b8fab05abc06c63d3c83c5bff941aa9
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 19:55:48 +0800

    Added a prompt asking user to enter a number.

---
+++ batch_4/number_with_most_duplicate.py
@@ -0,0 +1,4 @@
+#  A program that keeps asking the user for a number until they enter an invalid input. Then, show the number that appeared the most.
+
+num = float(input("Enter a number: "))
+print(num)
