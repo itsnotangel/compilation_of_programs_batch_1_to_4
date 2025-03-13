5da3a1dfbd3680edd27498338b40f5950b6c54bd
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-13 18:50:07 +0800

    Added a list to store inputted numbers.

--- batch_4/highest_to_lowest_number.py
+++ batch_4/highest_to_lowest_number.py
@@ -1,4 +1,6 @@
 # A program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest.
 
+num_list = []
 num = float(input("Enter a number: "))
-print(num)
+num_list.append(num)
+print(num_list)
